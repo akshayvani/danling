09def19e24603c7e450d990be2040b3e326deeb4
HP <HP@DESKTOP-MHU59BL>
2022-08-29 21:11:47 +0530

    [akshay]3rd  change project

---
+++ src/danling1/class2.java
@@ -0,0 +1,9 @@
+package danling1;
+
+public class class2 
+{
+    public static void main(String[] args)
+    {
+		System.out.println("SHREE RAMLING VARAD SHREE DANLING PRASANNA");
+	}
+}
---
+++ src/danling1/class3.java
@@ -0,0 +1,19 @@
+package danling1;
+
+public class class3
+{
+       public static void main(String[] args)
+       {
+		
+    	   System.out.println("akshay");
+    	   System.out.println("amar");
+    	   System.out.println("amol");
+    	   System.out.println("aparna");
+    	   System.out.println("ashwini");
+    	   
+    	   
+    	   
+    	   
+    	   
+	}
+}
